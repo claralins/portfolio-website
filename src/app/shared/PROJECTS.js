846979d5d3f84263ea45874935f121b4d8f26e10
claralins <77737793+claralins@users.noreply.github.com>
2022-07-12 14:04:28 -0400

    Update PROJECTS.js

--- src/app/shared/PROJECTS.js
+++ src/app/shared/PROJECTS.js
@@ -28,24 +28,4 @@ export const PROJECTS = [
 
     image: "../images/yoga.jpg",
   },
-  {
-    id: 3,
-    name: "Example1",
-    tech: ["NodeJS", ""],
-    description:
-      "Sed congue at dui egestas molestie. In pellentesque sollicitudin mauris. Etiam viverra sodales metus ac ornare.",
-    image: "../images/yoga.jpg",
-    longDescription:
-      "Sed congue at dui egestas molestie. In pellentesque sollicitudin mauris. Etiam viverra sodales metus ac ornare.",
-  },
-  {
-    id: 4,
-    name: "Example2",
-    tech: ["NodeJS", ""],
-    description:
-      "Etiam eget condimentum felis. Fusce aliquam eros nec tellus tempus ultrices. ",
-    image: "../images/yoga.jpg",
-    longDescription:
-      "Sed congue at dui egestas molestie. In pellentesque sollicitudin mauris. Etiam viverra sodales metus ac ornare.",
-  },
 ];
